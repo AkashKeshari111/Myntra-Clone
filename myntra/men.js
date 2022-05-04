ddd912101443e2f1e3d7e50ab0aba049cb0b431f
AtulPandey429 <pandeyatul429x@gmail.com>
2022-05-04 10:28:16 +0530

    kids

---
+++ myntra/men.js
@@ -0,0 +1,362 @@
+var A_shirt1= [
+   { "url1" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/7488102/2019/8/22/8002a744-0dad-4dbb-9481-cf0090134c3b1566454086786-Dennis-Lingo-Men-Pink-Slim-Fit-Solid-Casual-Shirt-9891566454-1.jpg"},
+   { "url2" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/7488102/2019/8/22/9292c168-14fc-45b0-94d7-442414f8d4f81566454086728-Dennis-Lingo-Men-Pink-Slim-Fit-Solid-Casual-Shirt-9891566454-4.jpg"},
+   { "url3" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/7488102/2019/8/22/b40e3089-2e16-4982-b6e9-9e5522d6725b1566454086712-Dennis-Lingo-Men-Pink-Slim-Fit-Solid-Casual-Shirt-9891566454-5.jpg"},
+
+]
+var A_shirt2= [
+    { "url1" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/9260157/2019/6/1/7e59185c-0a70-4c35-88f4-314a3e8610e11559376771551-HERENOW-Men-Shirts-5171559376770146-1.jpg"},
+    { "url2" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/9260157/2019/6/1/14527757-9da9-440c-b164-e17ca544e1d61559376771492-HERENOW-Men-Shirts-5171559376770146-4.jpg"},
+    { "url3" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/9260157/2019/6/1/3d64b990-f3e3-4038-ab71-9a06a71dbe9c1559376771478-HERENOW-Men-Shirts-5171559376770146-5.jpg"},
+ 
+ ]
+ var A_shirt3= [
+    { "url1" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/7488103/2019/8/22/acfba45f-8f9c-4b97-b5bc-a909418bdf4c1566454100620-Dennis-Lingo-Men-Green-Slim-Fit-Solid-Casual-Shirt-358156645-3.jpg"},
+    { "url2" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/7488103/2019/8/22/a158d792-8e7d-44ed-a5bf-5b229dc5c98c1566454100598-Dennis-Lingo-Men-Green-Slim-Fit-Solid-Casual-Shirt-358156645-4.jpg"},
+    { "url3" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/7488103/2019/8/22/b7868593-94f4-4b6e-97de-931733a74a0f1566454100581-Dennis-Lingo-Men-Green-Slim-Fit-Solid-Casual-Shirt-358156645-5.jpg"},
+ 
+ ]
+ var A_shirt4= [
+    { "url1" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2050688/2018/2/5/11517823120338-HIGHLANDER-Men-White-Slim-Fit-Solid-Casual-Shirt-5911517823120124-1.jpg"},
+    { "url2" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2050688/2018/2/5/11517823120287-HIGHLANDER-Men-White-Slim-Fit-Solid-Casual-Shirt-5911517823120124-4.jpg"},
+    { "url3" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2050688/2022/1/25/09d1916a-220b-4ee6-aaa4-1be6b0ddd1911643088426039-HIGHLANDER-Men-White-Slim-Fit-Casual-Shirt-6141643088425880-5.jpg"},
+ 
+ ]
+ var A_shirt5= [
+    { "url1" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/12313422/2020/12/15/bacbb39f-3e52-4fbc-92fd-162889cf76df1608006543670-Calvin-Klein-Jeans-Men-Shirts-6571608006542098-1.jpg"},
+    { "url2" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/12313422/2020/12/15/7f2516cf-8691-480d-ba68-22ecfd2082381608006543617-Calvin-Klein-Jeans-Men-Shirts-6571608006542098-2.jpg"},
+    { "url3" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/12313422/2020/12/15/a64e3def-c0d8-475e-8a17-0949ed039a021608006543468-Calvin-Klein-Jeans-Men-Shirts-6571608006542098-5.jpg"},
+ 
+ ]
+ var A_shirt6= [
+    { "url1" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11780956/2020/4/3/f724f764-2aae-4918-ba70-6a20335f90451585905028495HIGHLANDERMenWhiteBlueSlimFitPrintedCasualShirt1.jpg"},
+    { "url2" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11780956/2020/4/3/b229264f-b742-4250-a5d3-019a251311f61585905028659HIGHLANDERMenWhiteBlueSlimFitPrintedCasualShirt4.jpg"},
+    { "url3" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11780956/2020/4/3/fc923952-7172-468e-adbc-77b1d961774f1585905028770HIGHLANDERMenWhiteBlueSlimFitPrintedCasualShirt6.jpg"},
+ 
+ ]
+ var A_shirt7= [
+    { "url1" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11361282/2020/2/14/8bec485e-80f3-41b0-808f-bd1f87311c8a1581676447021-WROGN-Men-Shirts-9611581676445142-4.jpg"},
+    { "url2" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11361282/2020/2/14/e711e654-2680-4d5b-8ffb-e615e29c78c31581676446983-WROGN-Men-Shirts-9611581676445142-5.jpg"},
+    { "url3" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11361282/2020/2/14/84a51bf7-9a44-4cbe-8807-97ca11480e261581676446941-WROGN-Men-Shirts-9611581676445142-6.jpg"},
+ 
+ ]
+ var A_shirt8= [
+    { "url1" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1364628/2016/8/31/11472636737718-Roadster-Men-Blue-Regular-Fit-Printed-Casual-Shirt-6121472636737160-1.jpg"},
+    { "url2" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1364628/2016/8/31/11472636737656-Roadster-Men-Blue-Regular-Fit-Printed-Casual-Shirt-6121472636737160-4.jpg"},
+    { "url3" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1364628/2016/8/31/11472636737673-Roadster-Men-Blue-Regular-Fit-Printed-Casual-Shirt-6121472636737160-3.jpg"},
+ 
+ ]
+ var A_shirt9= [
+    { "url1" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2284691/2018/1/29/11517205743041-Roadster-Men-Black-Regular-Fit-Solid-Casual-Shirt-2871517205742822-1.jpg"},
+    { "url2" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2284691/2018/1/29/11517205742955-Roadster-Men-Black-Regular-Fit-Solid-Casual-Shirt-2871517205742822-4.jpg"},
+    { "url3" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2284691/2018/1/29/11517205742926-Roadster-Men-Black-Regular-Fit-Solid-Casual-Shirt-2871517205742822-5.jpg"},
+ 
+ ]
+ var A_shirt10= [
+    { "url1" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2347555/2019/12/3/77c23017-fd9d-4ad8-95f5-8722154e85ac1575361938857-HIGHLANDER-Men-White--Blue-Slim-Fit-Striped-Casual-Shirt-304-1.jpg"},
+    { "url2" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2347555/2019/12/3/bd14082b-e0e1-41a2-b2a7-e1e52b3ac3c31575361938813-HIGHLANDER-Men-White--Blue-Slim-Fit-Striped-Casual-Shirt-304-2.jpg"},
+    { "url3" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2347555/2019/12/3/fe600427-0de0-4e5f-aeac-b8223c6c5f6d1575361938653-HIGHLANDER-Men-White--Blue-Slim-Fit-Striped-Casual-Shirt-304-5.jpg"},
+ 
+ ]
+ var A_shirt11= [
+    { "url1" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1729391/2019/8/28/a9d4a7aa-af7e-4a37-a9d4-a94dc8c057e81566992341974-WROGN-Navy-Blue-Slim-Fit-Checked-Casual-Shirt-52415669923403-1.jpg"},
+    { "url2" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1729391/2019/8/28/29ab8ff4-aece-4f9c-80f7-6ab0dbf3772a1566992341955-WROGN-Navy-Blue-Slim-Fit-Checked-Casual-Shirt-52415669923403-2.jpg"},
+    { "url3" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1729391/2019/8/28/b11e16ca-ac5c-4e9f-9376-1e3f4e420a391566992341904-WROGN-Navy-Blue-Slim-Fit-Checked-Casual-Shirt-52415669923403-5.jpg"},
+ 
+ ]
+ var A_shirt12= [
+    { "url1" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/15353964/2021/12/29/5b122d9d-d028-42b8-b05f-9872dffe6f8e1640780629629-Nautica-Men-Shirts-4121640780629118-1.jpg"},
+    { "url2" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/15353964/2021/12/29/b2959dd5-c9ca-44ac-b6e8-d62f007aaf531640780629570-Nautica-Men-Shirts-4121640780629118-4.jpg"},
+    { "url3" : "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/15353964/2021/12/29/9f2ad271-3622-4351-ba03-efd272ec07d51640780629528-Nautica-Men-Shirts-4121640780629118-6.jpg"},
+ 
+ ]
+ var A_shirt13= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt14= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt15= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt16= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt17= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt18= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt19= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt20= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt21= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt22= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt23= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt24= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt25= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt26= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt27= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt28= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt29= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt30= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt31= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt32= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt33= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt34= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt35= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt36= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt37= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt38= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt39= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt40= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt41= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt42= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt43= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt44= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt45= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt46= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt47= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt48= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt49= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt50= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt51= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt52= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt53= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt54= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt55= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt56= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt57= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt58= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt59= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+ var A_shirt60= [
+    { "url1" : ""},
+    { "url2" : ""},
+    { "url3" : ""},
+ 
+ ]
+
+ 
