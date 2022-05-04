ced3c3ff60a79d04ebd00e61a53f198ff537f2c0
AkashKeshari111 <akashkeshari2722000@gmail.com>
2022-05-04 19:07:35 +0530

    tshirt ready for other product

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,7 @@
 import {navbar} from "../components/navbar.js"
 document.getElementById("navbar").innerHTML=navbar();
 
-
-
-import { footer } from "../components/footer.js";
+  import { footer } from "../components/footer.js";
 document.getElementById("footerDiv").innerHTML=footer();
 
-
+  
